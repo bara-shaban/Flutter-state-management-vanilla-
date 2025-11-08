a85e87c95e4d9cb1ccb456a26a95e71eb2be324a
Bara Shaban <barashaban03@gmail.com>
2025-11-08 20:37:47 +0300

    lerning the riverpod 3 and setting up other stuff and testing in genrale

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,168 @@
 // ignore_for_file: unreachable_from_main, public_member_api_docs
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hooks_riverpod/legacy.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
   runApp(
-    ProviderScope(
-      child: const App(),
+    const ProviderScope(
+      child: App(),
     ),
   );
 }
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home Page')),
+    );
+  }
+}
+
+class DataModel extends Notifier<List<Person>> {
+  @override
+  List<Person> build() => [];
+
+  void addPerson(Person person) {
+    state = [...state, person];
+  }
+
+  void removePerson(Person person) {
+    state = state.where((p) => p.uuid != person.uuid).toList();
+  }
+
+  void updatePerson(Person updatedPerson) {
+    state = state.map((p) {
+      if (p.uuid == updatedPerson.uuid) {
+        return p.copyWith(
+          name: updatedPerson.name,
+          age: updatedPerson.age,
+        );
+      }
+      return p;
+    }).toList();
+  }
+}
+
+final peopleProvider = NotifierProvider<DataModel, List<Person>>(() {
+  return DataModel();
+});
+
+@immutable
+class Person {
+  Person({
+    required this.age,
+    required this.name,
+    String? uuid,
+  }) : uuid = uuid ?? const Uuid().v4();
+
+  final String uuid;
+  final int age;
+  final String name;
+
+  Person copyWith({
+    int? age,
+    String? name,
+  }) {
+    return Person(
+      uuid: uuid,
+      age: age ?? this.age,
+      name: name ?? this.name,
+    );
+  }
+
+  String get displayName => 'Person(name: $name, age: $age)';
+
+  @override
+  String toString() => 'Person(uuid: $uuid, name: $name, age: $age)';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+}
+ */
+
+
+/* 
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentWeather = ref.watch(
+      weatherProvider,
+    );
+    return Scaffold(
+      appBar: AppBar(title: const Text('Weather')),
+      body: Column(
+        children: [
+          currentWeather.when(
+            data: (data) => Text(data, style: const TextStyle(fontSize: 40)),
+            loading: () => const Padding(
+              padding: EdgeInsets.all(8),
+              child: CircularProgressIndicator(),
+            ),
+            error: (error, stack) => const Text('Error loading weather'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: City.values.length,
+              itemBuilder: (context, index) {
+                final city = City.values[index];
+                final isSelected = city == ref.watch(currentCityProvider);
+                return ListTile(
+                  title: Text(
+                    city.toString(),
+                  ),
+                  trailing: isSelected ? const Icon(Icons.check) : null,
+                  onTap: () =>
+                      ref.read(currentCityProvider.notifier).state = city,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
 
 extension OptionalInfixAddition<T extends num> on T? {
   T? operator +(T? other) {
@@ -58,60 +211,4 @@ final weatherProvider = FutureProvider<WeatherEmoji>((ref) async {
     return unknownWeatherEmoji;
   }
 });
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends ConsumerWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final currentWeather = ref.watch(
-      weatherProvider,
-    );
-    return Scaffold(
-      appBar: AppBar(title: const Text('Weather')),
-      body: Column(
-        children: [
-          currentWeather.when(
-            data: (data) => Text(data, style: const TextStyle(fontSize: 40)),
-            loading: () => const Padding(
-              padding: EdgeInsets.all(8),
-              child: CircularProgressIndicator(),
-            ),
-            error: (error, stack) => const Text('Error loading weather'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: City.values.length,
-              itemBuilder: (context, index) {
-                final city = City.values[index];
-                final isSelected = city == ref.watch(currentCityProvider);
-                return ListTile(
-                  title: Text(
-                    city.toString(),
-                  ),
-                  trailing: isSelected ? const Icon(Icons.check) : null,
-                  onTap: () =>
-                      ref.read(currentCityProvider.notifier).state = city,
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+ */
